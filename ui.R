c7506d4e594040ac9a0f62f46da7a4b98148785c
kregtt <jean-luc.lheveder@insee.fr>
2026-03-13 19:33:14 +0000

    20260313

--- ui.R
+++ ui.R
@@ -11,17 +11,61 @@ library(plyr)
 
 
 library(sf)
-depm <- read_sf("dep_France_DOM.shp")
+
+
+test_result_path <- "dep_France_DOM.shp"
+
+get_object("kregtt/trav/dep_France_DOM.shp"
+           , bucket = Sys.getenv("S3_BUCKET"),region="") %>%
+  writeBin(test_result_path)
+
+
+
+test_result_path <- "dep_France_DOM.shx"
+
+get_object("kregtt/trav/dep_France_DOM.shx"
+           , bucket = Sys.getenv("S3_BUCKET"),region="") %>%
+  writeBin(test_result_path)
+
+
+
+
+test_result_path <- "www/dep_France_DOM.prj"
+
+get_object("kregtt/trav/dep_France_DOM.prj"
+           , bucket = Sys.getenv("S3_BUCKET"),region="") %>%
+  writeBin(test_result_path)
+
+
+test_result_path <- "www/dep_France_DOM.cpg"
+
+get_object("kregtt/trav/dep_France_DOM.cpg"
+           , bucket = Sys.getenv("S3_BUCKET"),region="") %>%
+  writeBin(test_result_path)
+
+
+test_result_path <- "www/dep_France_DOM.dbf"
+
+get_object("kregtt/trav/dep_France_DOM.dbf"
+           , bucket = Sys.getenv("S3_BUCKET"),region="") %>%
+  writeBin(test_result_path)
+
+
+
+depm <- read_sf("www/dep_France_DOM.shp")
 depm<-subset(depm,reg=="53")
-comm <- read_sf("com_France_DOM.shp")
-depm<-subset(depm,reg=="53")
+# comm <- read_sf("com_France_DOM.shp")
+# comm<-subset(comm,reg=="53")
 getwd()
 comm <-s3readRDS("trav/comm.rds", bucket = "kregtt",region="")
-
+comm[is.na(comm)] <- ""
 
 #pal <- colorNumeric("Greens", domain = comm$type) ## create a color palette
-bins <- c(0, 1, 2, 3, Inf)
-pal <- colorBin("YlOrRd", domain = comm$type, bins = bins)
+# bins <- c(0, 1, 2, 3, Inf)
+# pal <- colorBin("YlOrRd", domain = comm$type, bins = bins)
+
+pal <- colorNumeric(c("white","red", "green", "blue"), comm$type)
+
 # library(rmapshaper)
 # commb <- comm %>% 
 #   # Simplifier la géométrie pour une carte plus légère
@@ -30,12 +74,17 @@ pal <- colorBin("YlOrRd", domain = comm$type, bins = bins)
 commb <- comm %>%
   sf::st_transform('+proj=longlat +datum=WGS84')
 
+depm <- depm %>%
+  sf::st_transform('+proj=longlat +datum=WGS84')
+
+ 
 
 map_regions <- leaflet() %>%
   addTiles() %>% 
+  addPolygons(data=depm, weight= 3,col= "black",opacity = 0.7) %>% 
   addPolygons(
     data = commb,
-    label = ~libelle,
+    label = ~paste0(" ", libelle," ",lib," ",nom," "),
     popup = ~paste0(" ", libelle," ",lib," ",nom," "),
     color = "#444444", weight = 1, smoothFactor = 0.5,
      opacity = 1.0, fillOpacity = 0.5,
@@ -44,7 +93,15 @@ map_regions <- leaflet() %>%
                                          bringToFront = TRUE))  %>% 
   addLegend(
   title = "type",
-  pal = pal, values = commb$type)
+  values = commb$type,
+  colors = c("white","red", "green", "blue"),
+  labels = c("", "dem", "prix", "bi-réseau"),
+  
+  ) 
+
+
+
+
 
 map_regions
 
